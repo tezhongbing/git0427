aefe8b6742a6a92dfb16e3bff6a0ae518d74e585
tezhongbing <1041010782@qq.com>
2016-09-18 21:08:45 +0800

    za
    za

--- src/com/atguigu/git/Student.java
+++ src/com/atguigu/git/Student.java
@@ -5,6 +5,7 @@ public class Student {
 	public static void main(String[] args) {
 		System.out.println("欢迎来到德莱联盟~");
 		System.out.println("hello 服务器");
+		System.out.println("欢迎来到哈哈哈德莱联盟~");
 	}
 
 }
